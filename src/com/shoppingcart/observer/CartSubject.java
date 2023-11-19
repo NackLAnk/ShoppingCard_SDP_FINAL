38ab5eea13df8bffb71fdd0c3c2abd5b4a4e6614
Alisher Tungyshbay <www.at.7777@gmail.com>
2023-11-19 22:37:42 +0600

    v1

--- src/com/shoppingcart/observer/CartSubject.java
+++
@@ -1,7 +0,0 @@
-package com.shoppingcart.observer;
-
-public interface CartSubject {
-    void addObserver(CartObserver observer);
-    void removeObserver(CartObserver observer);
-    void notifyObservers();
-}
